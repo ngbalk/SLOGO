5129a520b3995e7816a8fa81ab09ae9cf8a3c387
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro.local>
2014-10-03 18:24:29 -0400

    added Canvas class

---
+++ src/application/Canvas.java
@@ -0,0 +1,26 @@
+package application;
+
+import com.sun.javafx.geom.Point2D;
+
+
+public class Canvas{
+
+	public Canvas() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * Draw on the Canvas from the starting Point2D "start" to the ending Point2D "end"
+	 * @param start Point2D
+	 * @param end Point2D
+	 */
+	public void draw(Point2D start, Point2D end){
+		
+	}
+	/**
+	 * Clear the current canvas of any drawings
+	 */
+	public void clear(){
+		
+	}
+
+}
