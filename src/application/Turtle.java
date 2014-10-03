eabc0705e891bcb9052e13945b829e3f6b6d078f
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro.local>
2014-10-03 18:18:04 -0400

    added Turtle Class

---
+++ src/application/Turtle.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Turtle {
+
+	public Turtle() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/application/Workspace.java
+++ src/application/Workspace.java
@@ -7,11 +7,30 @@ public class Workspace {
 	public Workspace() {
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * pass the Workspace the List<Action> of Actions, and the Workspace will handle applying these Actions to the member
+	 * Turtle.These Actions need to take in a Turtle as a parameter and must know how to call Turtle.move(double distance) 
+	 * and Turtle.rotate(double degrees);
+	 * @param actionChain a List<Action> of Actions which will be applied in succession to our current Turtle.
+	 */
 	public void updateTurtle(List<Action> actionChain){
 		
 	}
+	/**
+	 * This method handles displaying this specific Workspace, pushing it to the front of the display and making it the current 
+	 * Workspace.
+	 * @return
+	 */
 	public Group display(){
 		return new Group();
+	}
+	/**
+	 * This method hides this specific Workspace, pushing it to the back and make the next Workspace in the Queue the current 
+	 * Workspace.
+	 * @return
+	 */
+	public void hide(){
+		
 	}
 
 }
