7ba0e9370fe40d4cdc94f6482f325f7905e69e4c
Pranava Raparla <pranava.raparla@gmail.com>
2014-10-04 10:48:34 -0400

    Created RandomNode

---
+++ src/application/slogonode/MathOperations/RandomNode.java
@@ -0,0 +1,23 @@
+/**
+ *  @author Pranava Raparla
+ *  Created: October 4th, 2014
+ *  Modified: October 4th, 2014
+ */
+package application.slogonode.MathOperations;
+
+import java.util.Random;
+
+public class RandomNode extends MathOperations {
+
+	public RandomNode() {
+		myType = "MathOperation";
+		myOperation = "RANDOM";
+	}
+
+	@Override
+	public int evaluate() {
+		Random rand = new Random();
+		return rand.nextInt(myChildren.get(0).evaluate());
+	}
+
+}
