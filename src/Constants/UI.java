247c4e12777d79b74a72b11eb69a9a3b220d32a3
Nick Balkissoon <nickbalkissoon@nicks-mbp.wireless.duke.local>
2014-10-06 13:27:22 -0400

    updated View

--- src/Constants/UI.java
+++ src/Constants/UI.java
@@ -4,6 +4,7 @@ public final class UI {
 	public static final String ROOT_LAYOUT_FXML_LOCATION = "RootLayout.fxml";
 	public static final int STAGE_WIDTH = 1000;
 	public static final int STAGE_HEIGHT = 1000;
+	public static final String STAGE_TITLE = "SLogo";
 	
 	
 	
--- src/application/View.java
+++ src/application/View.java
@@ -1,18 +1,34 @@
 package application;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import Constants.UI;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class View {
+	private Model myModel;
 	private Stage myStage;
 	private Scene myScene;
+	private TabPane myWorkspaceTabs;
+	private Workspace myActiveWorkspace;
+	private List<Workspace> myWorkspaces;
 
 	public View(Stage stage){
+		this.myModel = new Model();
+		this.myWorkspaces = new ArrayList<Workspace>();
 		myStage = stage;
 		this.initializeStage();
 	}
@@ -24,6 +40,13 @@ public class View {
 	 * @param errorString
 	 */
 	public void displayError(String errorString){
+		
+	}
+	/**
+	 * Update the currentWorkspace's turtle with the actionChain returned by Model.parseInput().
+	 * @param actionChain
+	 */
+	private void updateTurtle(List<Action> actionChain){
 		
 	}
 	/**
@@ -40,10 +63,50 @@ public class View {
 			}
 	        Scene scene = new Scene(root, Constants.UI.STAGE_HEIGHT, Constants.UI.STAGE_WIDTH);
 	        this.myScene = scene;
-	        myStage.setTitle("SLogo");
-	        myStage.setScene(scene);
-	        myStage.show();
+	        this.myStage.setTitle(UI.STAGE_TITLE);
+	        this.myStage.setScene(scene);
+	        this.myStage.show();
+	        this.myWorkspaceTabs = (TabPane) this.myScene.lookup("#workspaceTabs");
+	        Button newTab = (Button) this.myScene.lookup("#newWorkspace");
+	        newTab.setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+						addWorkspace(new Workspace());
+				}
+			});
+	        Button submitTextForParsing = (Button) this.myScene.lookup("#submitText");
+	        TextArea textInput = (TextArea) this.myScene.lookup("#textInput");
+	        submitTextForParsing.setOnAction(new EventHandler<ActionEvent>(){
+				@Override
+				public void handle(ActionEvent arg0) {
+					myModel.parseInput(textInput.getText());
+					textInput.clear();
+				}
+	        });
 	}
+	/**
+	 * Add a workspace, adding it to both the TabPane and the List of workspaces.
+	 * @param workspace
+	 */
+	private void addWorkspace(Workspace workspace){
+		this.myWorkspaces.add(workspace);
+		this.myActiveWorkspace = workspace;
+		Tab newTab = new Tab(("Workspace #" + (this.myWorkspaceTabs.getTabs().size() + 1)));
+		newTab.setContent(workspace.display());
+		newTab.setOnClosed((new EventHandler<Event>(){
+			@Override
+			public void handle(Event arg0) {
+				myWorkspaces.remove(workspace);
+			}
+		}));
+		newTab.setOnSelectionChanged(new EventHandler<Event>(){
+			@Override
+			public void handle(Event arg0) {
+				myActiveWorkspace = workspace;
+			}
+		});
+		this.myWorkspaceTabs.getTabs().add(newTab);
+	}
+
 
 }
 
