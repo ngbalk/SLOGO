bb6b5c74f3e5c8244ac341383dd0a473e7ac2aaa
Pranava Raparla <pranava.raparla@gmail.com>
2014-10-09 21:54:12 -0400

    Created move forward node.

---
+++ src/application/slogonode/TurtleCommands/MoveForwardNode.java
@@ -0,0 +1,30 @@
+/**
+ *  @author Pranava Raparla
+ *  Created: October 9th, 2014
+ *  Modified: October 9th, 2014
+ */
+package application.slogonode.TurtleCommands;
+
+import application.Actions.AbstractAction;
+import application.Actions.FwdAction;
+import application.slogonode.SLogoNode;
+
+public abstract class MoveForwardNode extends TurtleCommandsNode {
+	
+	public MoveForwardNode() {
+		super();
+		myCommand = "MoveForward";
+	}
+	
+	@Override
+	public int evaluate() {
+		if(myChildren.get(0)!=null) {
+			myValue = myChildren.get(0).evaluate();
+			AbstractAction action = new FwdAction(myValue);
+			myActions.add(action);
+		}
+		if(myChildren.get(1)!=null)
+			myChildren.get(1).evaluate();
+		return myValue;
+	}
+}
