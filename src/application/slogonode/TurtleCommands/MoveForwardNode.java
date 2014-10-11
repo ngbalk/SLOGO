c61f0c82add28522feb20121c8cf21016634c0a4
Pranava Raparla <pranava.raparla@gmail.com>
2014-10-11 02:51:20 -0400

    Fixed null pointer issue

--- src/application/slogonode/TurtleCommands/MoveForwardNode.java
+++ src/application/slogonode/TurtleCommands/MoveForwardNode.java
@@ -19,12 +19,14 @@ public class MoveForwardNode extends TurtleCommandsNode {
 	@Override
 	public int evaluate() {
 		for(int i=0; i<myChildren.size(); i++) {
-			if(myChildren.get(0)!=null) {
+			if(myChildren.get(i)==null)
+				continue;
+			else if(i==0) {
 				myValue = myChildren.get(0).evaluate();
 				AbstractAction action = new FwdAction(myValue);
 				myActions.add(action);
 			}
-			if(myChildren.get(1)!=null)
+			if(i==1)
 				myChildren.get(1).evaluate();
 		}
 		return myValue;
