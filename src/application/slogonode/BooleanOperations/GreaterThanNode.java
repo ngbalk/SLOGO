e2e48cc7b17913a627b447654cbc154d609ec07e
wsvalentine94 <wsv@duke.edu>
2014-10-24 02:38:21 -0400

    Revert "Equal,Greater"
    This reverts commit bc9ef5d95a6d9f09cc16380943f84da1e06a9c1c.

--- src/application/slogonode/BooleanOperations/GreaterThanNode.java
+++ src/application/slogonode/BooleanOperations/GreaterThanNode.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import application.Actions.AbstractAction;
-import application.slogonode.Number.ConstantNode;
+import application.Actions.ConstantAction;
 
 public class GreaterThanNode extends BooleanOperations{
 
@@ -15,10 +15,11 @@ public class GreaterThanNode extends BooleanOperations{
 	public List<AbstractAction> evaluate() {
 		List<AbstractAction> actionList = new ArrayList<AbstractAction>();
 		double value = 0;
-		if (myChildren.get(0).evaluate().get(0).getValue() > myChildren.get(1).evaluate().get(0).getValue()){
+		if (myChildren.get(0).evaluate().get(0).myValue > myChildren.get(1).evaluate().get(0).myValue){
 			value = 1;
 		}
-		actionList.addAll((new ConstantNode(value)).evaluate());
+		AbstractAction action = new ConstantAction(value);
+		actionList.add(action);
 		return actionList;
 	}
 }
