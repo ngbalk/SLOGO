c5338b2e625805a73771e5142658dc4001490a1c
monicchoe <mc348@duke.edu>
2014-10-24 02:06:02 -0400

    And

--- src/application/slogonode/BooleanOperations/AndNode.java
+++ src/application/slogonode/BooleanOperations/AndNode.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import application.Actions.AbstractAction;
-import application.Actions.ConstantAction;
+import application.slogonode.Number.ConstantNode;
 
 public class AndNode extends BooleanOperations {
 
@@ -12,15 +12,15 @@ public class AndNode extends BooleanOperations {
 		myOperation = "AND";
 	}
 
+	@Override
 	public List<AbstractAction> evaluate() {
 		List<AbstractAction> actionList = new ArrayList<AbstractAction>();
 		double value = 0;
-		if (myChildren.get(0).evaluate().get(0).myValue == 1
-				&& myChildren.get(1).evaluate().get(0).myValue == 1) {
+		if (myChildren.get(0).evaluate().get(0).getValue()== 1
+				&& myChildren.get(1).evaluate().get(0).getValue()== 1) {
 			value = 1;
 		}
-		AbstractAction action = new ConstantAction(value);
-		actionList.add(action);
+		actionList.addAll((new ConstantNode(value)).evaluate());
 		return actionList;
 	}
 }
