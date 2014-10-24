04adb277d1027c7ce397d683870268c3493be1be
monicchoe <mc348@duke.edu>
2014-10-24 05:36:38 -0400

    Turtle Queries Classes

---
+++ src/application/slogonode/TurtleQueries/XCorNode.java
@@ -0,0 +1,19 @@
+package application.slogonode.TurtleQueries;
+
+import java.util.List;
+
+import application.Actions.AbstractAction;
+
+public class XCorNode extends TurtleQueriesNode{
+	
+	public XCorNode(){
+		super();
+		myCommand = "XCor"; 
+	}
+
+	@Override
+	public List<AbstractAction> evaluate() {
+		
+		return null;
+	}
+}
