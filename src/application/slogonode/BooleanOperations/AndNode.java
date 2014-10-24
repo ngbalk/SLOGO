7637982ea7beff2fd540b8ff5705509af26067c9
monicchoe <mc348@duke.edu>
2014-10-24 01:56:21 -0400

    Fixing Boolean Nodes
    And, EqualTo, GreaterThan

--- src/application/slogonode/BooleanOperations/AndNode.java
+++ src/application/slogonode/BooleanOperations/AndNode.java
@@ -1,12 +1,25 @@
 package application.slogonode.BooleanOperations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import application.Actions.AbstractAction;
+import application.Actions.ConstantAction;
+
 public class AndNode extends BooleanOperations{
 
 	public AndNode() {
 		myOperation = "AND";
 	}
 
-	public int evaluate() {
-		return myChildren.get(0).evaluate()==1 && myChildren.get(1).evaluate()==1 ? 1 : 0;
+	public List<AbstractAction> evaluate() {
+		List<AbstractAction> actionList = new ArrayList<AbstractAction>();
+		double value = 0;
+		if (myChildren.get(0).evaluate().get(0).myValue==1 && myChildren.get(1).evaluate().get(0).myValue == 1){
+			value = 1;
+		}
+		AbstractAction action = new ConstantAction(value);
+		actionList.add(action);
+		return actionList;
 	}
 }
