f5e987ceebc7380f4a7d3b3bbe0050b31843414c
monicchoe <mc348@duke.edu>
2014-10-26 06:43:55 -0400

    Renaming

--- src/application/SLogoNodeFactory.java
+++ src/application/SLogoNodeFactory.java
@@ -27,7 +27,7 @@ public class SLogoNodeFactory {
 	public SLogoNode getSLogoNodeFromString(String nodeName) {
 		System.out.println("\nReached the factory!\n");
 		SLogoNode commandNode = null;
-		Map<String, String> map = new HashMap<String, String>();
+		Map<String, String> pathnamesMap= new HashMap<String, String>();
 		try {
 			try {
 				commandNode = (SLogoNode) getConstantSLogoNode(nodeName);
@@ -35,14 +35,14 @@ public class SLogoNodeFactory {
 			catch (Exception e) {
 				ResourceBundle classBundle = ResourceBundle.getBundle(GUIconstants.RESOURCE_FILE_PREFIX+"ClassType");
 				PropertiesFactory fact = new PropertiesFactory();
-				map = fact.getCommandsMap(classBundle);
+				pathnamesMap = fact.getCommandsMap(classBundle);
 				
 			}
 //			ResourceBundle classBundle = ResourceBundle.getBundle(GUIconstants.RESOURCE_FILE_PREFIX+"ClassType");
 //			PropertiesFactory fact = new PropertiesFactory();
 //			Map<String, String> map = new HashMap<String, String>();
 //			map = fact.getCommandsMap(classBundle);
-			Class classType = Class.forName(map.get(nodeName));
+			Class classType = Class.forName(pathnamesMap.get(nodeName));
 			commandNode = (SLogoNode) classType.newInstance();
 		} catch (Exception e) {
 			System.out.println("\nERROR! THIS COMMAND DOES NOT EXIST: " + nodeName);
